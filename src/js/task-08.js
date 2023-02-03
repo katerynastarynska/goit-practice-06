a3fffb1564a12d53e7b9080132622ecac7d35d54
Kateryna Starynska <katerynastarynska@gmail.com>
2023-02-02 18:20:08 -0600

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    const formElements = evt.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        email, 
+        password,
+    }
+
+    console.log(formData);
+    formEl.reset();
+}

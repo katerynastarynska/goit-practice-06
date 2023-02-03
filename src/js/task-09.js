4ba830a470697af0da5cb86e51dbdbec2a39b52e
Kateryna Starynska <katerynastarynska@gmail.com>
2023-02-03 14:45:49 -0600

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const colorChangeBtn = document.querySelector('.change-color');
+const colorEl = document.querySelector('.color');
+
+colorChangeBtn.addEventListener('click', onBtnChangeClick);
+
+function onBtnChangeClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = bodyEl.style.backgroundColor;
+}
